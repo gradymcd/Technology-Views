11e8698e8e9a0be0a5bdc1a6f1ee686a0b9bba68
Grady McDowell <gradyrmcd@gmail.com>
2015-12-14 10:50:51 -0500

    Changed index to links to submit and results

--- submit.php
+++ submit.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Technology Views - Home</title>
+	<title>Technology Views - Submit</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/paper/bootstrap.min.css">
 </head>
 <body>
-	<h1 class="well">
-		<a href="submit.php">Submit form</a>
-		<a href="results.php">View results</a>
-	</h1>
+<h1 class="well">
+	<a href="submit.php">Submit form</a>
+	<a href="results.php">View results</a>
+</h1>
 
 </body>
 </html>
