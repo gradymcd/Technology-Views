b0289125340a8017db9bd438344a8d2d5a64f1fb
Grady McDowell <gradyrmcd@gmail.com>
2015-12-14 10:47:51 -0500

    Initial files and basic template

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Technology Views - Submit</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/paper/bootstrap.min.css">
+</head>
+<body>
+	<p>Test</p>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Technology Views - Results</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/paper/bootstrap.min.css">
+</head>
+<body>
+
+</body>
+</html>
