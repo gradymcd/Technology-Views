e4441332be4f6e84b07354d119dc2170b4aeb64f
Grady McDowell <gradyrmcd@gmail.com>
2015-12-16 09:49:38 -0500

    Made form an actual form

--- submit.php
+++ submit.php
@@ -1,5 +1,5 @@
 <?php
-	if (isset($_GET['submitted'])) {
+	if (isset($_POST['submitted'])) {
 		header('Location: results.php');
 	}
 ?>
@@ -11,7 +11,7 @@
 </head>
 <body>
 <div class="container">
-	<form role="form">
+	<form role="form" action="submit.php" method="post">
 		<?php
 			include('db.php');
 			$result = $db->query("SELECT * from `data`");
