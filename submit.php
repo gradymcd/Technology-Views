1fa42f8275194b98b94b730b2f46647073365f31
Grady McDowell <gradyrmcd@gmail.com>
2015-12-16 09:41:28 -0500

    Fixed some styling issues

--- submit.php
+++ submit.php
@@ -10,20 +10,23 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/paper/bootstrap.min.css">
 </head>
 <body>
-<form role="form">
-	<?php
-		include('db.php');
-		$result = $db->query("SELECT * from `data`");
-		while ($row = $result->fetch_assoc()) {
-			echo('<div class="form-group">');
-			echo('<label for="' . $row['question'] . '"></label>');
-			$options = unserialize($row['choices']);
-			foreach($options as $key=>$value) {
-				echo('<input type="radio" name="' . $row['id'] . '" id="' . $row['id'] . $key .  '"');
+<div class="container">
+	<form role="form">
+		<?php
+			include('db.php');
+			$result = $db->query("SELECT * from `data`");
+			while ($row = $result->fetch_assoc()) {
+				echo('<div class="form-group">');
+				echo('<h3> ' . $row['question'] . ' </h3>');
+				echo('<label for="' . $row['question'] . '"></label>');
+				$options = unserialize($row['choices']);
+				foreach ($options as $key => $value) {
+					echo('<input type="radio" name="' . $row['id'] . '" id="' . $row['id'] . $key . '">');
+				}
+				echo('</div>');
 			}
-			echo('</div>');
-		}
-	?>
-</form>
+		?>
+	</form>
+</div>
 </body>
 </html>
