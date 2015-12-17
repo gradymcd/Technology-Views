acfd4f53467c8e786f9cb33a2dbbaca0d792337a
Grady McDowell <gradyrmcd@gmail.com>
2015-12-17 09:30:14 -0500

    Display percent instead of decimal

--- results.php
+++ results.php
@@ -23,8 +23,7 @@
 			}
 		?>
 	</div>
-	<script type="text/javascript"
-	        src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
 </body>
 </html>
